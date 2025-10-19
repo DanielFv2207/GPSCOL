64a6a941c7499ac90224893eb25b334ddda5884d
Daniel Forero <elfore305@gmail.com>
2025-10-19 00:12:32 -0500

    funcionalidad de login y registro de usuarios agregada

---
+++ Login/app.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('registerForm');
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = {
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+    };
+
+    const res = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await res.json();
+    
+
+    const message = document.getElementById('message');
+    message.textContent = result.message;
+    
+    if (result.success) {
+        //successful login
+        message.style.color = 'green';
+        window.location.href = '../index.html';
+    } else {
+        // login failed
+        message.style.color = 'red';
+    }
+});
---
+++ Login/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import cors from 'cors';
+import connection from './db.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//route for user registration
+app.post('/register', (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.json({ message: 'All fields are required' });
+    }
+
+    const sql = 'INSERT INTO usuarios (username, email, password) VALUES (?, ?, ?)';
+    connection.query(sql, [username, email, password], (err) => {
+    if (err) {
+        console.error('Error inserting user:', err);
+        return res.json({ success: false, message: 'Error registering user' });
+    }
+    res.json({ success: true, message: 'User registered successfully' });
+    });
+});
+
+//route for user login
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.json({ message: 'Email and password are required' });
+    }
+
+    const sql = 'SELECT * FROM usuarios WHERE email = ? AND password = ?';
+    connection.query(sql, [email, password], (err, results) => {
+        if (err) {
+            console.error('Error querying:', err);
+            return res.json({ message: 'Server error' });
+        }
+
+        if (results.length > 0) {
+        // User found
+            res.json({ success: true, message: 'Login successful', user: results[0] });
+        } else {
+        // No match with email or password
+        res.json({ success: false, message: 'Invalid email or password' });
+        }
+    });
+});
+
+app.listen(3000, () => console.log('Server running at http://localhost:3000'));
